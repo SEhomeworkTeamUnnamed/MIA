a4ac45901db851948ea42d3bd46550761374cd8f
AlbertChang0217 <1301649399@qq.com>
2015-12-08 22:00:22 +0800

    just to test the window frame.

---
+++ src/GUI/GUIsmall/testframe.java
@@ -0,0 +1,45 @@
+package GUI.GUIsmall;
+
+import GUI.MainPane;
+import MathFunc.FuncClass;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultTreeModel;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Created by 张思源 on 2015/12/8.
+ */
+public class testframe extends JFrame {
+    testframe(/*DefaultTreeModel treeModel, JTree tree, FuncClass RootClass, */String FrameName){
+
+        super(FrameName);
+
+        //getContentPane().add(new MainPane(treeModel, tree, RootClass), BorderLayout.CENTER);
+        getContentPane().add(new testpane());
+
+        pack();
+        setVisible(true);
+        //设置Frame位置
+        Toolkit kit=Toolkit.getDefaultToolkit();
+        Dimension screenSize=kit.getScreenSize();
+        setLocation(screenSize.width/4,screenSize.height/4);
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                //int option =
+                //        JOptionPane.showConfirmDialog(null, "是否保存更改?", "",
+                //                JOptionPane.YES_NO_CANCEL_OPTION);
+                //System.out.print(option);
+                System.exit(0);
+            }
+        });
+    }
+
+
+    public static void main(String[] args){
+        new testframe("hhh");
+    }
+}
---
+++ src/GUI/GUIsmall/testpane.java
@@ -0,0 +1,38 @@
+package GUI.GUIsmall;
+
+import GUI.LeftPane;
+import GUI.RightPane;
+import MathFunc.FuncClass;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultTreeModel;
+import java.awt.*;
+
+/**
+ * Created by 张思源 on 2015/12/8.
+ */
+public class testpane extends JSplitPane{
+    testpane(){
+        super();
+
+        setOneTouchExpandable(true);
+        setContinuousLayout(true);
+        setPreferredSize(new Dimension(600, 400));
+        setOrientation(JSplitPane.VERTICAL_SPLIT);
+
+        JButton b1=new JButton("添加函数类");
+        b1.setEnabled(true);
+
+        Container con=new Container();
+        con.setLayout(new FlowLayout());
+        con.add(b1);
+
+        setTopComponent(con);
+
+        /*setLeftComponent(new LeftPane(tree));
+        setRightComponent(new RightPane(treeModel,tree,RootClass));*/
+
+        setDividerSize(3);
+        setDividerLocation(150);
+    }
+}
